b6c93ba88c012f8077f6a720b975c1bc72671897
Yuanchen Lu <yuanchen-lu@Yuanchens-MacBook-Pro.local>
2018-06-23 13:18:15 -0400

    download and zip

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 var fs = require('fs');
 var git = require('nodegit');
 const {exec} = require('child_process');
-var sourceUrl = 'http://9.37.137.241:8000.Archive.zip';
+var sourceUrl = 'http://9.37.137.241:8000/Archive.zip';
+var path = 'tmp';
 
-function downloadAndUnzipFile() {
-    function download() {
-        //exec(file)
-    }
+function readDir(dir) {
+    list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        console.log(file);
+    });
 }
+function downloadAndUnzipFile() {
+    function unzip(zipName) {
+        var cmd = `unzip ${zipName} -d ${path}`;
+        //console.log(cmd);
+        exec(cmd, (err, stdout, stderr) => {
+            if (err) {
+              console.error(`exec error: ${err}`);
+              return;
+            }
+            console.log(`unzipped file ${zipName} to ${path}`);
+        });
+    }
+    function download(sourceUrl) {
+        var zipName;
+        //exec('rm *.zip.*');
+        exec(`wget ${sourceUrl} && (ls | grep *.zip)`, (err, stdout, stderr) => {
+            if (err) {
+              console.error(`exec error: ${err}`);
+              return;
+            }
+            zipName = stdout.trim();
+            console.log(`downloaded zip file: ${zipName}`);
+            // readDir('.');
+            unzip(zipName);
+        });
+    }
+    download(sourceUrl);
+}
+
+downloadAndUnzipFile();
