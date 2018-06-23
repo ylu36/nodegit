e52b35d35f6844c3b1cdb773a736a4e504ef541d
Yuanchen Lu <yuanchen-lu@Yuanchens-MacBook-Pro.local>
2018-06-23 12:43:14 -0400

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var git = require('nodegit');
+const {exec} = require('child_process');
+var sourceUrl = 'http://9.37.137.241:8000.Archive.zip';
+
+function downloadAndUnzipFile() {
+    function download() {
+        //exec(file)
+    }
+}
