9283b785646349fae815859603abb0978917f06e
Yuanchen Lu <yuanchen-lu@Yuanchens-MacBook-Pro.local>
2018-06-23 13:35:41 -0400

    can create .git if applicable

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var git = require('nodegit');
 const {exec} = require('child_process');
 var sourceUrl = 'http://9.37.137.241:8000/Archive.zip';
 var path = 'tmp';
-
+var isBare = 0; // lets create a .git subfolder
+var repofolder = 'tmp';
 function readDir(dir) {
     list = fs.readdirSync(dir);
     list.forEach(function(file) {
@@ -20,7 +21,9 @@ function downloadAndUnzipFile() {
               return;
             }
             console.log(`unzipped file ${zipName} to ${path}`);
+            process_git();
         });
+        exec(`rm ${zipName}`);
     }
     function download(sourceUrl) {
         var zipName;
@@ -37,6 +40,17 @@ function downloadAndUnzipFile() {
         });
     }
     download(sourceUrl);
+}
+
+function process_git() {
+    isBare = fs.existsSync(`${path}/.git`) == false? 0:1;
+    console.log(isBare);
+    git.Repository.init(path, isBare).then(function (repo) {
+       
+    })
+    .catch(function (reasonForFailure) {
+       console.log(reasonForFailure);
+      });
 }
 
 downloadAndUnzipFile();
